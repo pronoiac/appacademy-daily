eb9183a7b62da37aa8735b6fc3e3ff58da86b8e1
James Young <pronoiac@gmail.com>
2014-08-07 23:55:51 -0700

    word_chains.rb - phase IIa

--- w1d4/word_chains.rb
+++ w1d4/word_chains.rb
@@ -1,7 +1,7 @@
 require "set"
 
 class WordChainer
-  attr_reader :dictionary
+  attr_reader :dictionary, :current_words, :all_seen_words
   
   def initialize
     @dictionary = []
@@ -10,7 +10,7 @@ class WordChainer
     # read dictionary into @dictionary, a set 
     File.foreach(dictionary_file) do |word|
       word.chomp!
-      @dictionary << word
+      dictionary << word
     end
   end 
   
@@ -29,6 +29,30 @@ class WordChainer
     end # i
     results.to_a
   end # /adjacent_words
+  
+  def run(source, target)
+    @current_words = [source]
+    @all_seen_words = [source]
+    @new_current_words = []
+    steps = 1
+    until @current_words.empty? 
+      @new_current_words = []
+      @current_words.each do |word|
+        # puts "current word: #{word}"
+        adjacent_words(word).each do |adjacent|
+          next if @all_seen_words.include?(adjacent)
+          @all_seen_words << adjacent
+          @new_current_words << adjacent
+        end
+      end
+      print "Step #{steps}, #{@new_current_words.count} new words, "
+      puts "#{current_words.count} words total previously: \n"
+      puts "#{@new_current_words}"
+      puts
+      @current_words = @new_current_words
+      steps += 1
+    end
+  end
 
   
 end
@@ -41,6 +65,15 @@ def testing
   
   puts "adj: swam"
   p chainer.adjacent_words "swam"
+  
+  # puts "swam -> ruby?"
+  # p chainer.run "swam", "ruby"
+  
+  # puts "rows -> tail?"
+  puts "vent -> beam? (slow - 4 min?)"
+  p chainer.run "vent", "beam"
+  
+  # p chainer.run "chunder", "plunder"
 end # /testing
 
 testing
