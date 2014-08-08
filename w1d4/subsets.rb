d421852287730c5c4dd33223998f199dad84f72f
James Young <pronoiac@gmail.com>
2014-08-07 22:45:03 -0700

    subsets.rb - meets spec

--- w1d4/subsets.rb
+++ w1d4/subsets.rb
@@ -1,28 +1,27 @@
+require 'set'
+
 def subsets(full_set)
-  results = []
+  #puts "full set: #{full_set}"
+  results = Set.new [full_set]
   results << [] # let's just add the 0-length subset
-  # outer loop: 1st item of subset.
+  # results << [full_set]
+  # let's see. how about, we remove one item at a time, 
+  # then recurse on that set, combining the results?
   (0...full_set.length).each do |i|
-    puts "start with # #{i}"
-    # inner loop: length of set, 1 to remaining length of array
-    # let's see. say array of 3, starting with 1, should be (1..2)
-    # or, do absolute positions
-    # (1...full_set.length - i).each do |slice_length|
-    (i+1..full_set.length).each do |j|
-      # get slice
-      slice = full_set[i..j]
-      # seen this slice already? 
-      results.push slice unless results.include? slice
-    end # inner loop      
-  end # outer loop
-  results 
+    # puts "removing entry # #{i}"
+    truncated = full_set.dup
+    truncated.delete_at(i)
+    # puts "truncated: #{truncated}"
+    results = results + subsets(truncated)
+  end 
+  results.to_a
 end # /subsets
 
 def testing
   test_array = [1, 2, 3]
   puts "test array: #{test_array}"
   puts "subsets:"
-  p subsets(test_array)
+  p subsets(test_array).sort
 end
 
 testing
