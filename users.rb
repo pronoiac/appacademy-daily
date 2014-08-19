5b1da8c2943f56423d00f1a8005a75bdcbd649a5
James Young <pronoiac@gmail.com>
2014-08-19 13:58:04 -0700

    user - added easy methods

--- users.rb
+++ users.rb
@@ -1,13 +1,9 @@
 
 
 class User
-  def initialize(options={})
-    @id    = options['id']
-    @fname = options['fname']
-    @lname = options['lname']
-  end
-  
-  def find_by_id(id)
+  attr_reader :id, :fname, :lname
+
+  def self.find_by_id(id)
     query = <<-SQL
       SELECT
           *
@@ -18,8 +14,57 @@ class User
     SQL
     results = QuestionsDatabase.instance.execute(query, id)
     
-    # results.map { |result| User.new(result) }
     User.new(results.first)
   end
+  
+  def self.find_by_name(fname, lname)
+    query = <<-SQL
+      SELECT
+          *
+      FROM
+          users
+      WHERE
+          fname = (?)
+          AND lname = (?)
+    SQL
+    results = QuestionsDatabase.instance.execute(query, fname, lname)
+    
+    User.new(results.first)
+  end
+
+  def initialize(options={})
+    @id    = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+  
+  def authored_questions
+    query = <<-SQL
+      SELECT
+          *
+      FROM
+          questions
+      WHERE
+          author_id = (?)
+    SQL
+    
+    QuestionsDatabase.instance.execute(query, @id)    
+  end
+  
+  def authored_replies
+    query = <<-SQL
+      SELECT
+          *
+      FROM
+          replies
+      WHERE
+          author_id = (?)
+    SQL
+  
+    QuestionsDatabase.instance.execute(query, @id)    
+  end
+  
+  
+    
 end
 
